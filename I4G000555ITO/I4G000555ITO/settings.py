728d0ff9da9ee3407dd4227238d887cb8da16756
codeIsmaeel <debolaqasim@gmail.com>
2022-06-16 14:41:27 +0100

    applying changes

--- I4G000555ITO/I4G000555ITO/settings.py
+++ I4G000555ITO/I4G000555ITO/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z5ixajj0%(nqd1fkv&az&_%vx1cu1kbp1oj@6wt005=-8_9!t3'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEBUG'
 
 ALLOWED_HOSTS = []
 
